d95e818b413156b40b339d7eb45d937e54395113
BOSCH Luckas <boschluckas@gmail.com>
2023-02-28 21:04:49 +0100

    update

--- main.ps1
+++ main.ps1
@@ -1,52 +1,3 @@
-<#
-.SYNOPSIS
-    Digitally signs PS1 files in a folder using a certificate.
-
-.DESCRIPTION
-    This script digitally signs all PS1 files in a folder using a certificate. The script can sign all files in the folder recursively. It can also import and export the certificate and change the certificate's password.
-
-.PARAMETER sign
-    Specifies the path to the folder containing the PS1 files to sign.
-
-.PARAMETER recursive
-    Specifies whether the script should sign all files in the folder recursively.
-
-.PARAMETER certificat
-    Specifies the name of the certificate to use for signing the files. Default is "certificat.pfx".
-
-.PARAMETER doc
-    Displays detailed documentation for the script.
-
-.PARAMETER help
-    Displays help for the script.
-
-.PARAMETER version
-    Displays the version of the script.
-
-.PARAMETER output
-    Specifies the path to save the signed files.
-
-.PARAMETER password
-    Specifies the password for the certificate.
-
-.PARAMETER import
-    Specifies the location to import the certificate to. Default is "Cert:\LocalMachine\Root".
-
-.PARAMETER export
-    Exports the certificate to a .pfx file.
-
-.EXAMPLE
-    pwsign.ps1 -sign C:\Scripts -recursive -certificat MyCert.pfx -output C:\SignedScripts -password MyPassword
-
-    This command signs all PS1 files in the C:\Scripts folder and its subfolders using the MyCert.pfx certificate with the password MyPassword. The signed files are saved in the C:\SignedScripts folder.
-
-.NOTES
-    Author: Your Name
-    Version: 1.0
-#>
-
-
-
 param(
     #folder to sign
     [alias("s")]
@@ -58,7 +9,7 @@ param(
 
     #Create certificat to sign 
     [alias("c")]
-    [string]$certificat = "certificat.pfx",
+    [string]$certificat,
 
     #Get documentation about pwsign script 
     [alias("d")]
@@ -69,124 +20,152 @@ param(
 
     #get script version
     [alias("v")]
-    [string]$version = "Version 0.0.1",
+    [switch]$version,
 
     #output path
     [alias("o")]
-    [switch]$output,
+    [string]$output,
 
-    #set certificat password
-    [alias("p")]
-    [switch]$password,
+    #output path
+    [alias("g")]
+    [switch]$generate,
+
 
     #import certificat in local computer
     # here user can define store location
     [alias("i")]
-    [string]$import="Cert:\LocalMachine\Root",
+    [switch]$import,
 
     #export certificat
     #user will choose save path
-    [alias("e")]
-    [switch]$export
+    [alias("p")]
+    [string]$path,
+
+    [alias("a")]
+    [string]$authority
+    
+
 )
 
-if ($help) {
-    Get-Help pwsign.ps1 -Full
-    return
+# Condition pour le paramètre -c
+if([string]::IsNullOrEmpty($certificat)) {
+    $certificat = "certificat.pfx"
 }
 
-if ($doc) {
-    Get-Help pwsign.ps1 -Detailed
-    return
+# Condition pour le paramètre -d
+if(!$doc) {
+    $doc = $false
 }
 
-if ($version) {
-    Write-Output $version
-    return
+# Condition pour le paramètre -v
+if(!$version) {
+    $version = $false
 }
 
-if ($certificat -eq "certificat.pfx" -and !$password) {
-    Write-Output "You must specify a password for the certificate."
-    return
+if(!$generate) {
+    $generate = $false
+}
+# Condition pour le paramètre -i
+if(!$import) {
+    $import 
 }
 
-if ($sign) {
-    if ($recursive) {
-        Sign-FilesRecursively -FolderPath $sign -CertificatPath $certificat
-    } else {
-        Sign-FilesInFolder -FolderPath $sign -CertificatPath $certificat
-    }
+# Condition pour le paramètre -e
+if(!$export) {
+    $export = $false
 }
 
-if ($export) {
-    Export-Certificate -CertPath $certificat
-}
 
-if ($import) {
-    Import-Certificate -CertPath $certificat -StoreLocation $import
-}
-
-if ($certificat -ne "certificat.pfx" -and $password) {
-    Set-CertificatePassword -CertPath $certificat
-}
-
-function Sign-FilesInFolder {
+#function to recursively sign every file in folder
+function signRecursively {
     param(
-        [string]$FolderPath,
-        [string]$CertificatPath
+        [string]$certificat,
+        [string]$sign
     )
 
-    # Get a list of all PS1 files in the folder
-    $files = Get-ChildItem -Path $FolderPath -Filter "*.ps1"
 
-    # For each PS1 file, sign it with the specified certificate
+        # Spécifiez le chemin d'accès au dossier contenant les fichiers PS1 à signer
+    $folderPath = "$sign"
+
+    # Récupérer la liste de tous les fichiers PS1 dans le dossier et ses sous-dossiers
+    $files = Get-ChildItem -Path $folderPath -Recurse -Filter "*.ps1"
+
+    # Pour chaque fichier PS1, signer numériquement le script avec le certificat spécifié
     foreach ($file in $files) {
-        Set-AuthenticodeSignature -FilePath $file.FullName -Certificate (Get-ChildItem -Path cert:\CurrentUser\My\$CertificatPath)
+        Set-AuthenticodeSignature -FilePath $file.FullName -Certificate (Get-ChildItem -Path Cert:\LocalMachine\My\$certificat)
     }
+    
 }
 
-function Sign-FilesRecursively {
+function certifGenerate {
     param(
-        [string]$FolderPath,
-        [string]$CertificatPath
+        [string]$certificat,
+        [string]$path
     )
-
-    # Get a list of all PS1 files in the folder and its subfolders
-    $files = Get-ChildItem -Path $FolderPath -Recurse -Filter "*.ps1"
-
-    # For each PS1 file, sign it with the specified certificate
-    foreach ($file in $files) {
-        Set-AuthenticodeSignature -FilePath $file.FullName -Certificate (Get-ChildItem -Path cert:\CurrentUser\My\$CertificatPath)
+    if(!$path) {
+        $path = "C:"
     }
+    if(!$certificat) {
+        $certificat = "certificat.pfx"
+    }
+
+
+    $cert = New-SelfSignedCertificate -Type CodeSigningCert -DnsName "$certificat" -CertStoreLocation Cert:\LocalMachine\My -NotAfter (Get-Date).AddYears(1)
+    Export-Certificate -Cert $cert -FilePath "$path\$certificat"
+    
+    
+    return $cert
+
+
 }
 
-function Export-Certificate {
+
+function certifImport {
     param(
-        [string]$CertPath
+        [string]$authority,
+        [string]$path,
+        [string]$certificat
     )
+    if (!$authority) {
+        $authority = "Cert:\LocalMachine\Root"
+    }
+    if (!$path) {
+        $path = "C:"
+    }
+    if (!$certificat) {
+        $certificat = "certificat.pfx"
+    }
 
-    $path = Read-Host "Enter the path to save the certificate"
-    $password = Read-Host "Enter the password for the"
-    Export-PfxCertificate -Cert cert:\CurrentUser\My\$CertPath -FilePath $path -Password (ConvertTo-SecureString $password -AsPlainText -Force)
+    Import-Certificate -FilePath "$path\$certificat" -CertStoreLocation "$authority"
+
+
 }
 
-function Import-Certificate {
-    param(
-        [string]$CertPath,
-        [string]$StoreLocation
-    )
 
-    Import-PfxCertificate -FilePath $CertPath -CertStoreLocation $StoreLocation
+if($help) {
+    # Appeler la fonction qui affiche l'aide
+    Write-Host "Affichage de l'aide"
+}
+elseif($doc) {
+    # Appeler la fonction qui affiche la documentation
+    Write-Host "Affichage de la documentation"
+}
+elseif($version) {
+    # Afficher la version du script
+    Write-Host "Version du script: $version"
+}
+elseif($import) {
+    # Appeler la fonction de génération de certificat si l'argument -c est fourni
+    certifImport $authority $path $certificat
+}
+elseif($generate) {
+    Write-Host "Generation de certificat"
+    # Appeler la fonction de génération de certificat si l'argument -c est fourni
+    certifGenerate $certificat $path
+}
+else {
+    # Afficher un message d'erreur si aucun argument n'est fourni
+    Write-Host "Aucun argument fourni. Utilisez -help pour afficher l'aide."
 }
 
-function Set-CertificatePassword {
-    param(
-        [string]$CertPath
-    )
 
-    $password = Read-Host "Enter the new password for the certificate"
-    $newCert = Export-PfxCertificate -Cert cert:\CurrentUser\My\$CertPath | Import-PfxCertificate -Password (ConvertTo-SecureString $password -AsPlainText -Force) -CertStoreLocation cert:\CurrentUser\My
-    Remove-Item -Path cert:\CurrentUser\My\$CertPath
-    $newCertThumbprint = $newCert.Thumbprint
-    Write-Output "The password for the certificate has been changed. The new thumbprint is $newCertThumbprint."
-}
